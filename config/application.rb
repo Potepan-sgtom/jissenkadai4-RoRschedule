4b3d187fda3074265b39336b269fdbb184f87a01
EC2 Default User <ec2-user@ip-172-31-17-9.ec2.internal>
2022-06-19 06:52:08 +0000

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module PotepanRails
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Tokyo'#←追記(時間設定)
+    config.i18n.default_locale = :ja
   end
 end
